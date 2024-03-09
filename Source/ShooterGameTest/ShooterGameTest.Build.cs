1c508e39385d662df7e172ea1d59e0f701c60014
kennetha123 <jodysag5@gmail.com>
2024-03-09 19:42:50 +0700

    feat: initial code commit

---
+++ Source/ShooterGameTest/ShooterGameTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ShooterGameTest : ModuleRules
+{
+	public ShooterGameTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
